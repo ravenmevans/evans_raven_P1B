b3a962d514112640f1227d16de81e63fd12539a5
Raven M. Evans <ravenmevans@gmail.com>
2018-03-06 06:15:21 -0600

    jQuery Edits
    I couldn't remember how to change the background when you hovered over the tab so I choose to do a gallery slide show...still messed that up.

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-$(document).ready(function(){
-    $("'.info-box button").clicked(function){
-        $(this).toggleClass(".visible");
-        if( $(this).hasClass(".visible") ){
-            $(this).html("Show Less");
-        }else{
-            $(this).html("Show More");
-        }
-       
-        $(this).next().toggleClass(".visible");
+$(document).ready( function() {
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
     }
-}
+
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1} 
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none"; 
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block"; 
+      dots[slideIndex-1].className += " active";
+    }
+});
